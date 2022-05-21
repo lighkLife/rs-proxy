f233f4a2fe1d2633b9ee5db10d69eeb93e351bd8
lighk <daoshibushi@gmail.com>
2022-05-21 19:29:06 +0800

    feat(service): communication between client and target

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,79 @@
-use std::io::{BufReader, BufWriter, Read};
-use std::net::{IpAddr, SocketAddrV4, TcpListener, TcpStream};
-use std::str::FromStr;
+use std::io::{Result};
+use std::net::{SocketAddrV4, TcpListener, TcpStream};
+use std::{io, thread};
+use std::sync::Arc;
 use clap::Parser;
 
 #[macro_use]
 extern crate log;
 
-/// proxy
+const LISTENER_PORT_DEFAULT: u16 = 21883;
+
+/// a simple tcp proxy service
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// listen port
+    /// local listen port, default 21883, example: 31883
     #[clap(short, long)]
     port: Option<u16>,
 
-    /// forward to
+    /// target server address, example: 192.168.0.10:1883
     #[clap(short, long)]
-    forward: String,
+    target: String,
 }
+
 
 fn main() {
     env_logger::init();
 
-    let args :Args = Args::parse();
-    let port = args.port.unwrap_or(20080);
-    let addr: SocketAddrV4 = args.forward.parse().unwrap();
+    let args: Args = Args::parse();
+    let port = args.port.unwrap_or(LISTENER_PORT_DEFAULT);
+    let target_addr: SocketAddrV4 = args.target.parse()
+        .expect("Invalid Socket address");
+    let listen_addr = "127.0.0.1:".to_string() + &port.to_string();
 
-    info!("Start rs-proxy... ");
-    info!("listen {}, forward {}", port, addr);
+    info!("rs-proxy starting... ");
+    info!("listen to {}, target address is {}", listen_addr, target_addr);
 
-    match start_service(port, addr) {
+    match start_service(listen_addr, target_addr) {
         Err(e) => {
             error!("Start rs-proxy failed. {}", e)
         }
         _ => {}
     }
-
 }
 
-fn start_service(port: u16, addr: SocketAddrV4) -> std::io::Result<()>{
-    let listener = TcpListener::bind("127.0.0.1" + port)?;
-    for stream in listener.incoming() {
-        handle_client(stream?);
+fn start_service(listen_addr: String, target_addr: SocketAddrV4) -> Result<()> {
+    let listener = TcpListener::bind(&listen_addr)
+        .expect("Failed start Listener");
+    let target_stream = TcpStream::connect(target_addr)
+        .expect("Failed connect target host.");
+    info!("rs-proxy started.");
+
+    for listen_stream in listener.incoming() {
+        handle_client(listen_stream?, &target_stream)?;
     }
     Ok(())
 }
 
-fn handle_client(stream: TcpStream) {
-    let peer = stream.peer_addr()?;
-    info!("Connection established from {}", &peer);
-    let reader = BufReader::new(&stream);
-    let mut writer = BufWriter::new(&stream);
+fn handle_client(listen_stream: TcpStream, target_stream: &TcpStream) -> Result<()> {
+    info!("Connection established from {}", listen_stream.peer_addr()?);
+    info!("Connection established by {}", &target_stream.peer_addr()?);
+
+    let listen_arc = Arc::new(listen_stream);
+    let target_arc = Arc::new(target_stream);
+
+    let (mut listen_rx, mut listen_tx) = (listen_arc.try_clone()?, listen_arc.try_clone()?);
+    let (mut target_rx, mut target_tx) = (target_arc.try_clone()?, target_arc.try_clone()?);
+
+    let connections = vec![
+        thread::spawn(move || io::copy(&mut listen_rx, &mut target_tx).unwrap()),
+        thread::spawn(move || io::copy(&mut target_rx, &mut listen_tx).unwrap()),
+    ];
+
+    for handle in connections {
+        handle.join().unwrap();
+    }
+    Ok(())
 }
 
